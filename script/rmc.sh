ec58bbad22705381dd9ab4901c146c50020f4312
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-04-11 21:43:21 +0200

    Update rmc.sh

--- script/rmc.sh
+++ script/rmc.sh
@@ -11,9 +11,8 @@ for j in `seq 1 $clustercount`
 			ssh root@$i kubectl config use-context kind-cluster$clustername
 			ssh root@$i helm repo update
 			ssh root@$i helm install cilium cilium/cilium --version 1.11.2 --namespace kube-system --set cluster.name=cluster$clustername --set cluster.id=$clustername
-			ssh root@$i mv /root/.kube/config /root/.kube/cluster$clustername
+			ssh root@$i cp /root/.kube/config /root/.kube/cluster$clustername
 			ssh root@$i scp /root/.kube/cluster$clustername root@$managemodel:/root/.kube
-			ssh root@$i mv /root/.kube/cluster$clustername /root/.kube/config 
 			clustername=$(($clustername+1))	
 		done
 echo "----------------$i OK----------------"
