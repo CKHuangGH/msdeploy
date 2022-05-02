4e4b5962ff4f1ad4afb9e64b683de6c0cdb4015d
CKHuangGH <chihkaihuang1@gmail.com>
2022-05-02 13:55:22 +0200

    Update rmc.sh

--- script/rmc.sh
+++ script/rmc.sh
@@ -5,12 +5,12 @@ managemodel=$4
 yaml=$5
 for j in `seq 1 $clustercount`
 		do
-			ssh root@$i kind create cluster --image kindest/node:v1.23.3 --config /root/mck8s_lsv/remote_script/config_03/kind-example-config-$j.yaml --name cluster$clustername
+			ssh root@$i kind create cluster --image kindest/node:v1.23.6 --config /root/mck8s_lsv/remote_script/config_03/kind-example-config-$j.yaml --name cluster$clustername
 			echo "waiting cluster........"
 			sleep 15
 			ssh root@$i kubectl config use-context kind-cluster$clustername
 			ssh root@$i helm repo update
-			ssh root@$i helm install cilium cilium/cilium --version 1.11.2 --namespace kube-system --set cluster.name=cluster$clustername --set cluster.id=$clustername
+			ssh root@$i helm install cilium cilium/cilium --version 1.11.4 --namespace kube-system --set cluster.name=cluster$clustername --set cluster.id=$clustername
 			ssh root@$i mv /root/.kube/config /root/.kube/cluster$clustername
 			ssh root@$i scp /root/.kube/cluster$clustername root@$managemodel:/root/.kube
 			clustername=$(($clustername+1))	
