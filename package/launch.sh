7dbddee728374f7573755c62b361bd429c4a0b51
CKHuangGH <chihkaihuang1@gmail.com>
2023-07-18 16:30:41 +0200

    Update launch.sh

--- package/launch.sh
+++ package/launch.sh
@@ -1,6 +1,7 @@
 sudo sysctl fs.inotify.max_user_watches=524288
 sudo sysctl fs.inotify.max_user_instances=512
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 mv /root/package/kubectl /usr/local/bin/kubectl
 chmod 777 /usr/local/bin/kubectl
 
