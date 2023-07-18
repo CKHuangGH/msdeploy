9b839aea037c82a26091fbd73b33f78fa5c9db53
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-18 23:16:28 +0200

    Update init.sh

--- package/init.sh
+++ package/init.sh
@@ -7,3 +7,8 @@ sudo apt install make -y
 curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 mv /root/package/kubectl /usr/local/bin/kubectl
 chmod 777 /usr/local/bin/kubectl
+
+for i in {1..3}
+do
+echo "  apiServerAddress: \"$(ifconfig eno1 |grep "inet " | cut -f 10 -d " ")"\" >> config/kind-example-config-$i.yaml
+done
