c690f5a57f924c6e5c22d546bb64062b53d42ecd
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-18 18:32:37 +0200

    test

--- package/launch.sh
+++ package/launch.sh
@@ -5,6 +5,10 @@ curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 mv /root/package/kubectl /usr/local/bin/kubectl
 chmod 777 /usr/local/bin/kubectl
 
-kind create cluster --image kindest/node:v1.27.3 --name management0 &
-kind create cluster --image kindest/node:v1.27.3 --name management1 &
-kind create cluster --image kindest/node:v1.27.3 --name member0 &
+# kind create cluster --image kindest/node:v1.27.3 --name management0 &
+# kind create cluster --image kindest/node:v1.27.3 --name management1 &
+# kind create cluster --image kindest/node:v1.27.3 --name member0 &
+
+kind create cluster --config config/kind-example-config-1.yaml --name management0 &
+kind create cluster --config config/kind-example-config-1.yaml --name management1 &
+kind create cluster --config config/kind-example-config-1.yaml --name member0 &
