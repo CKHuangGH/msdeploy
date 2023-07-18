4ac9b78aacd900b60a85c92ff909367ee3e5ba17
CKHuangGH <chihkaihuang1@gmail.com>
2023-07-18 15:48:31 +0200

    Update remote_cluster.sh

--- remote_cluster.sh
+++ remote_cluster.sh
@@ -1,5 +1,6 @@
 uniq $OAR_NODEFILE > node_list
 manage=$(awk NR==1 node_list)
 
+ssh root@$manage rm -rf /root/package
 scp -r package root@$manage:/root/package
 ssh root@$manage chmod 777 -R /root/package
