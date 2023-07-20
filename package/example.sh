87aa79ca93fda8c509c19e3aa51232d20a0be01c
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-20 23:28:55 +0200

    test

---
+++ package/example.sh
@@ -0,0 +1,4 @@
+. /0.sh
+clusteradm create work my-first-work -f example/example1.yaml --clusters cluster1
+. /1.sh
+clusteradm create work my-first-work -f example/example2.yaml --clusters cluster2
