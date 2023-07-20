5f1023c28fa091021575ed1122f6a97474e08f57
CKHuangGH <chihkaihuang1@gmail.com>
2023-07-20 15:30:26 +0200

    Update ocm.sh

--- package/ocm.sh
+++ package/ocm.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Deploy Prometheus Federation on Cluster 0
-kubectl config use-context kind-management0
-
-clusteradm init --wait --context kind-management0
+clusteradm init --wait --context kind-management0 &
+clusteradm init --wait --context kind-management1 &
