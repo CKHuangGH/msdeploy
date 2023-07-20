34a2adfc803f8eb755ef20bb11140240c178d3d3
CKHuangGH <chihkaihuang1@gmail.com>
2023-07-20 13:44:55 +0200

    123

--- package/launch.sh
+++ package/launch.sh
@@ -3,5 +3,5 @@
 # kind create cluster --image kindest/node:v1.27.3 --name member0 &
 
 kind create cluster --config config/kind-example-config-1.yaml --name management0 &
-# kind create cluster --config config/kind-example-config-2.yaml --name management1 &
+kind create cluster --config config/kind-example-config-2.yaml --name management1 &
 kind create cluster --config config/kind-example-config-3.yaml --name member0 &
