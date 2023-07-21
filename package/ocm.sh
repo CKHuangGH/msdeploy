1e04fbc7d0e0771bc1fa9c6d7fb97b503bdbb8c9
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-21 20:22:44 +0200

    test

--- package/ocm.sh
+++ package/ocm.sh
@@ -2,3 +2,7 @@
 
 clusteradm init --wait --context kind-management0 &
 clusteradm init --wait --context kind-management1 &
+
+kubectl --context=kind-management0 create -f metrics_server.yaml
+kubectl --context=kind-management1 create -f metrics_server.yaml
+kubectl --context=kind-member0 create -f metrics_server.yaml
