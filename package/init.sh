c8d67fd6ce6edd6e3af98e10d4c6daedfec3144f
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-31 17:26:12 +0200

    Update init.sh

--- package/init.sh
+++ package/init.sh
@@ -8,6 +8,8 @@ curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 mv /root/package/kubectl /usr/local/bin/kubectl
 chmod 777 /usr/local/bin/kubectl
 
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
+
 for i in {1..3}
 do
 echo "  apiServerAddress: \"$(ifconfig eno1 |grep "inet " | cut -f 10 -d " ")"\" >> config/kind-example-config-$i.yaml
