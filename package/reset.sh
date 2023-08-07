73f71af1a4ec49a5f00a1de08655d8db6fd22dff
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-07 21:43:56 +0200

    job ok

--- package/reset.sh
+++ package/reset.sh
@@ -1,3 +1,7 @@
 kind delete cluster --name management0
 kind delete cluster --name management1
 kind delete cluster --name member0
+
+kind create cluster --config config/kind-example-config-1.yaml --name management0 &
+kind create cluster --config config/kind-example-config-2.yaml --name management1 &
+kind create cluster --config config/kind-example-config-3.yaml --name member0 &
