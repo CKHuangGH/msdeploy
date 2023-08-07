f5bdd2cf54e12d0dd30fced04a8feea96c40ac8e
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-07 17:23:00 +0200

    et

---
+++ package/rm-test.sh
@@ -0,0 +1,21 @@
+clusteradm delete work namespace-test --clusters cluster1
+
+clusteradm delete work job-test --clusters cluster1
+clusteradm delete work pod-test --clusters cluster1
+
+clusteradm delete work daemoset-test --clusters cluster1
+
+
+clusteradm delete work deployment-test --clusters cluster1
+clusteradm delete work service-nodeport-test --clusters cluster1
+
+clusteradm delete work endpointslice-test --clusters cluster1
+clusteradm delete work ingress-test --clusters cluster1
+
+clusteradm delete work pv-test --clusters cluster1
+clusteradm delete work pvc-test --clusters cluster1
+
+clusteradm delete work cfmap-test --clusters cluster1
+clusteradm delete work cfmap-run-test --clusters cluster1
+
+clusteradm delete work secret-test --clusters cluster1
