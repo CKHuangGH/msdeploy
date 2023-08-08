e111df1d2a6c4225f7257a7e2587cd7dcd496906
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-08 21:56:58 +0200

    test

--- package/example/03_show.sh
+++ package/example/03_show.sh
@@ -1,13 +1,24 @@
+echo "-----------------------namespace-----------------------"
 kubectl get namespace --context kind-member0
+echo "-----------------------job-----------------------"
 kubectl get job --context kind-member0
+echo "-----------------------pod-----------------------"
 kubectl get pod --context kind-member0
+echo "-----------------------deployment-----------------------"
 kubectl get deployment --context kind-member0
+echo "-----------------------service-----------------------"
 kubectl get service --context kind-member0
-kubectl get endpointslices --context kind-member0
+echo "-----------------------endpoint-----------------------"
 kubectl get endpoint --context kind-member0
+echo "-----------------------ingresses-----------------------"
 kubectl get ingresses --context kind-member0
+echo "-----------------------pv-----------------------"
 kubectl get pv --context kind-member0
+echo "-----------------------pvc-----------------------"
 kubectl get pvc --context kind-member0
+echo "-----------------------configmap-----------------------"
 kubectl get configmap --context kind-member0
+echo "-----------------------secret-----------------------"
 kubectl get secret --context kind-member0
+echo "-----------------------storageclass-----------------------"
 kubectl get storageclass --context kind-member0
