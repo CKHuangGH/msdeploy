953f296d4a608720e9b2a0dc5578e0cf464e8a1d
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-10 18:38:02 +0200

    eee

---
+++ package/fault/cicd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf fault-detection-system-mmc/
+
+git clone https://github.com/CKHuangGH/fault-detection-system-mmc.git
+chmod 777 fault-detection-system-mmc/
--- package/fault/fault.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf fault-detection-system-mmc/
-
-git clone https://github.com/CKHuangGH/fault-detection-system-mmc.git
-chmod 777 fault-detection-system-mmc/
-docker build -t chuangtw/faultdetection .
-docker login
-docker push chuangtw/faultdetection
