c21a59522a92cea6bf38c6e6d9de83db3f0b4760
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-11 16:32:29 +0200

    Update 03_show.sh

--- package/example/03_show.sh
+++ package/example/03_show.sh
@@ -1,24 +1,20 @@
 echo "-----------------------namespace-----------------------"
 kubectl get namespace --context kind-member0
-echo "-----------------------job-----------------------"
-kubectl get job --context kind-member0
 echo "-----------------------pod-----------------------"
-kubectl get pod --context kind-member0
-echo "-----------------------deployment-----------------------"
-kubectl get deployment --context kind-member0
+kubectl get pod -A --context kind-member0
 echo "-----------------------service-----------------------"
-kubectl get service --context kind-member0
+kubectl get service -A --context kind-member0
 echo "-----------------------endpoint-----------------------"
-kubectl get endpoint --context kind-member0
+kubectl get endpoint -A --context kind-member0
 echo "-----------------------ingresses-----------------------"
-kubectl get ingresses --context kind-member0
+kubectl get ingresses -A- -context kind-member0
 echo "-----------------------pv-----------------------"
 kubectl get pv --context kind-member0
 echo "-----------------------pvc-----------------------"
-kubectl get pvc --context kind-member0
+kubectl get pvc -A --context kind-member0
 echo "-----------------------configmap-----------------------"
-kubectl get configmap --context kind-member0
+kubectl get configmap -A --context kind-member0
 echo "-----------------------secret-----------------------"
-kubectl get secret --context kind-member0
+kubectl get secret -A --context kind-member0
 echo "-----------------------storageclass-----------------------"
 kubectl get storageclass --context kind-member0
