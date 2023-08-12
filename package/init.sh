58684c491760f9ff49553ebb9dc0e8aa99fe1977
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-12 19:56:35 +0200

    Update init.sh

--- package/init.sh
+++ package/init.sh
@@ -4,6 +4,8 @@ sudo sysctl fs.inotify.max_user_instances=512
 sudo apt install net-tools -y
 sudo apt install make -y
 
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
 curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 mv /root/package/kubectl /usr/local/bin/kubectl
 chmod 777 /usr/local/bin/kubectl
